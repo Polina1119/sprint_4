4669ce5c2738a2da89dad453a86cb5bebb1751f1
Polina Ermakova <polina1041@mail.ru>
2023-09-21 20:32:18 +0500

    first commit

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -18,7 +19,101 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize('name, genre', [
+        ['a', 'Фантастика'], ['a'*2,'Ужасы'], ['a'*39, 'Детективы'], ['a'*40, 'Мультфильмы']
+    ]
+                             )
+    def test_set_book_genre_four_books_increased_dictionary(self, name, genre):
+
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.books_genre[name] == genre
+
+    def test_get_book_genre_one_book_received_genre(self):
+
+        collector = BooksCollector()
+
+        name = 'Гордость и предубеждение и зомби'
+        genre = 'Фантастика'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_book_genre(name) == genre
+
+    @pytest.mark.parametrize('name, genre', [
+        ['Гордость и предубеждение и зомби', 'Фантастика'],
+        ['Релиз вовремя', 'Фантастика']
+    ]
+                             )
+    def test_get_books_with_specific_genre_two_books_name_book(self, name, genre):
+
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_books_with_specific_genre(genre) == [name]
+
+    def test_get_books_genre_one_book_received_dictionary(self):
+
+        collector = BooksCollector()
+
+        name = 'Гордость и предубеждение и зомби'
+        genre = 'Фантастика'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_books_genre() == {name : genre}
+
+    def test_get_books_for_children_one_book_received_name_book(self):
+
+        collector = BooksCollector()
+
+        name = 'Гордость и предубеждение и зомби'
+        genre = 'Фантастика'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_books_for_children() == [name]
+
+    def test_add_book_in_favorites_one_book_received_name_book(self):
+
+        collector = BooksCollector()
+
+        name = 'Убийца внутри меня'
+        genre = 'Детективы'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        collector.add_book_in_favorites(name)
+
+        assert collector.favorites == [name]
+
+    def test_delete_book_from_favorites_one_book_decreased_dictionary(self):
+
+        collector = BooksCollector()
+
+        name = 'Убийца внутри меня'
+        genre = 'Детективы'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        collector.add_book_in_favorites(name)
+        collector.delete_book_from_favorites(name)
+
+        assert len(collector.favorites) == 0
+
+    def test_get_list_of_favorites_books_one_book_received_name_book(self):
+
+        collector = BooksCollector()
+
+        name = 'Убийца внутри меня'
+        genre = 'Детективы'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        collector.add_book_in_favorites(name)
+
+        assert collector.get_list_of_favorites_books() == [name]
